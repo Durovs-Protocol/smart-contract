0fcb94e40ea2ca22f0078f2547d242f7c673d81c
danilsudbin <danilsudbin@yandex.ru>
2024-07-12 12:03:11 +0300

    .

--- utils/test_data.ts
+++ utils/test_data.ts
@@ -3,6 +3,7 @@ export const gasFee: number = 0.2; // Стоимость газа
 export const liquidationRatio: number = 1.15;
 export const stabilityFeeRate: number = 0.05;
 export const liquidationFee: number = 0.05;
+export const minHealthRate: number = 2;
 
 /**
  * UX
