ef9b7250bf0e89e40052a31a7e272310507f2abc
Olya <olyaares@MacBook-Air.local>
2024-09-16 17:16:57 +0300

    refactor(BLC-1742): отформатировала и поправила  документацию, сделала билд для фронта

--- scripts/info_system.ts
+++ scripts/info_system.ts
@@ -8,13 +8,15 @@ import { Asset } from '../wrappers/Manager';
 export async function run(provider: NetworkProvider) {
 
     const { reservePool, manager } = await contracts(provider, provider.sender().address!!);
-    // await showPoolWallets(managerContract, 'manager');
-    // await showPoolWallets(poolContract, 'reservePool');
-    await showBalances(manager, 'manager');
-    await showBalances(reservePool, 'reservePool');
+
+    await showWallets(manager, 'manager');
+    await showWallets(reservePool, 'reservePool');
+    await showDeps(manager, 'manager')
+    await showDeps(reservePool, 'reservePool')
+
     log(
-        'Version:'+
-            '\npool: ' +
+        'Versions'.toUpperCase() +
+            '\npool   : ' +
             await reservePool.getVersion() +
             '\nmanager: ' +
             await manager.getVersion()
@@ -24,56 +26,41 @@ export async function run(provider: NetworkProvider) {
 
     log(
         'System setting information'.toUpperCase() +
-            '\nmax Amount           : ' +
-            settings.maxAmount +
-            '\nmin Delay      : ' +
+            '\nmin Delay           : ' +
             settings.minDelay +
-            ' (ton)' +
-            '\nnew Manager      : ' +
-            settings.newManager.toString(),
+            '\nnew Manager Address : ' +
+            settings.newManager.toString() +
+            '\nmax Execution Time  : ' +
+            settings.maxExecutionTime
     );
-    async function showBalances(contract: any, name: string) {
+    async function showWallets(contract: any, name: string) {
         const allBalances: Dictionary<Address, Asset> = await contract.getBalances();
         const toncoin = allBalances.get(Address.parse(assets[0].master))!!.toString();
         const stakedTON = allBalances.get(Address.parse(assets[1].master))!!.toString();
         const hipoStakedTON = allBalances.get(Address.parse(assets[2].master))!!.toString();
         const tonstakers = allBalances.get(Address.parse(assets[3].master))!!.toString();
-        console.log(allBalances)
         log(
-            `Assets in ${name}`.toUpperCase() + 'Staked TON pool wallet :    ' + stakedTON +
-                '' + '\nHipo Staked TON pool wallet: ' + hipoStakedTON +
-                '' + '\nTon Stakers pool wallet:       ' + tonstakers +
-                '' + '\nToncoin pool wallet:   ' + toncoin +
-                '',
+            `Assets in ${name}`.toUpperCase() + 
+                '\nStaked TON pool wallet     :' + stakedTON +
+                '\nHipo Staked TON pool wallet:' + hipoStakedTON +
+                '\nTon Stakers pool wallet    :' + tonstakers +
+                '\nToncoin pool wallet        :' + toncoin 
         );
     }
-    async function showPoolWallets(contract: any, name: string) {
-        const allAssets: Dictionary<Address, Asset> = await contract.getAssets();
-        const toncoin = allAssets.get(Address.parse(assets[0].master))?.poolWallet.toString();
-        const stakedTON = allAssets.get(Address.parse(assets[1].master))?.poolWallet.toString();
-        const hipoStakedTON = allAssets.get(Address.parse(assets[2].master))?.poolWallet.toString();
-        const tonstakers = allAssets.get(Address.parse(assets[3].master))?.poolWallet.toString();
-        log(
-            `Assets in ${name}`.toUpperCase() + 'Staked TON pool wallet :    ' + stakedTON +
-                '' + '\nHipo Staked TON pool wallet: ' + hipoStakedTON +
-                '' + '\nTon Stakers pool wallet:       ' + tonstakers +
-                '' + '\nToncoin pool wallet:   ' + toncoin +
-                '',
-        );
-    }
+
     async function showDeps(contract: any, name: string) {
         const deps = await contract.getDeps();
 
         log(
             `deps in ${name}`.toUpperCase() +
-                'manager :    ' +
+                '\nmanager    :' +
                 deps.manager.toString() +
-                '\nreservePool: ' +
+                '\nreservePool:' +
                 deps.reservePool.toString() +
-                '\nprofitPool:       ' +
+                '\nprofitPool :' +
                 deps.profitPool.toString() +
-                '\nstable   ' +
-                deps.usdton.toString(),
+                '\nstable     :' +
+                deps.stable.toString(),
         );
     }
 }
