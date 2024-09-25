1659ddf3cc1656f678abae540f11db4f3ebb5c68
danilsudbin <danilsudbin@yandex.ru>
2024-09-25 14:47:59 +0300

    .

--- blueprint.config.ts
+++ blueprint.config.ts
@@ -2,10 +2,10 @@ import { Config } from '@ton/blueprint';
 
 export const config: Config = {
     network: {
-        endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+        endpoint: 'https://toncenter.com/api/v2/jsonRPC',
         // endpoint:
         //     'https://ton.access.orbs.network/4410c0ff5Bd3F8B62C092Ab4D238bEE463E64410/1/testnet/toncenter-api-v2/jsonRPC',
-        type: 'testnet',
+        type: 'mainnet',
         version: 'v2',
         key: '0ca7be614616999e57472e6b36d730d6e2945c4bc52a60f4f03e2c8301322ed1',
     },
